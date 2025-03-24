5ee67ed639f6e985cbc35ff0f95eb4cf64d9cb58
kia-kashanchi <kia.kashanchi@sanofi.com>
2025-03-24 23:04:16 +0100

    Delete OnlyTerraform directory

--- OnlyTerraform/modules/vm/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "public_ip_address" {
-  description = "VM public IP address"
-  value       = azurerm_public_ip.vm_public_ip.ip_address
-}
-
-output "vm_id" {
-  description = "Virtual Machine ID"
-  value       = azurerm_linux_virtual_machine.linux_vm.id
-}
--- OnlyTerraform/modules/vnet/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vnet_id" {
-  description = "Virtual Network ID"
-  value       = azurerm_virtual_network.main_vnet.id
-}
-
-output "subnet_ids" {
-  description = "Map of subnet names to IDs"
-  value       = { for k, v in azurerm_subnet.network_subnets : k => v.id }
-}
